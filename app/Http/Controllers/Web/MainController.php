ff15c4a3a2f40dd02f5e52db8b6b3281adb305dd
Олег Зотов <olegzotov2016@gmail.com>
2021-01-29 17:45:18 +0300

    ADD job

---
+++ app/Http/Controllers/Web/MainController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers\Web;
+
+use App\Http\Controllers\Controller;
+use App\Jobs\LogJob;
+use Illuminate\Contracts\View\View;
+
+/**
+ * Class MainController
+ *
+ * @package App\Http\Controllers\Web
+ */
+class MainController extends Controller
+{
+    /**
+     * @return View
+     */
+    public function index(): View
+    {
+        LogJob::dispatch()->delay(10);
+
+        return view('welcome');
+    }
+}
